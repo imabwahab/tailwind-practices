4a770b15cd95c7fbef3ec550fa5aa273e6666fe5
imabwahab <imabwahab@gmail.com>
2025-06-29 06:35:50 +0500

    practice 1 , center a div.

---
+++ practice-1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div class="bg-slate-800 flex flex-col md:flex-row justify-center items-center h-screen gap-2">
+      <div class="bg-orange-400 w-40 h-16 text-center rounded-2xl font-bold ">div1</div>
+      <div class="bg-blue-200 w-40 h-16 text-center rounded-2xl text-lg text-sky-800 font-bold">div2</div>
+      <div class="bg-red-400 w-40 h-16 text-center rounded-2xl text-cyan-100 font-bold">div3</div>
+    </div>
+
+  )
+}
+
+export default App
